60d049c9a5ae4faf5978f867289289959c143624
Gayatri-Priyadarsini <88318175+Gayatri-Priyadarsini@users.noreply.github.com>
2023-01-08 23:37:59 +0530

    Update demo_script.js

--- demo_script.js
+++ demo_script.js
@@ -1,1 +1,7 @@
-document.cookie = "user2=John Doe";
+setCookie("user2", user, 365);
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
