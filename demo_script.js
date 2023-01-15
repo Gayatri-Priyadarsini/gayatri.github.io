10c70211e113aad3023e6e16411697aabf0a6c98
Gayatri-Priyadarsini <88318175+Gayatri-Priyadarsini@users.noreply.github.com>
2023-01-16 04:13:30 +0530

    Update demo_script.js

--- demo_script.js
+++ demo_script.js
@@ -1,9 +1,14 @@
-document.write(22);
+document.write(25);
 
-setCookie("cookie2","value2", 365);
-function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
+// setCookie("cookie2","value2", 365);
+// function setCookie(cname, cvalue, exdays) {
+//   const d = new Date();
+//   d.setTime(d.getTime() + (exdays*24*60*60*1000));
+//   let expires = "expires="+ d.toUTCString();
+//   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+// }
+
+fetch('http://localhost:9999/example',{
+method: ‘GET’,
+credentials: 'include'
+});
